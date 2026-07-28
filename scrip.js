ae44b3ab1fde62302391a3367ad195df1a31286a
GeraldSkym <pestano.gerald16@gmail.com>
2026-07-28 23:21:02 +0800

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,1 +1,49 @@
+const form =
+    document.getElementById(
+        "anniversaryForm"
+    );
 
+
+const dateInput =
+    document.getElementById(
+        "anniversaryDate"
+    );
+
+
+form.addEventListener(
+    "submit",
+    function (event) {
+
+        event.preventDefault();
+
+
+        if (dateInput.value === "") {
+
+            alert(
+                "Please enter our anniversary date ♥"
+            );
+
+            dateInput.focus();
+
+            return;
+        }
+
+
+        /*
+        Kapag may susunod ka nang page,
+        palitan ang alert ng:
+
+        window.location.href = "story.html";
+        */
+
+
+        alert(
+
+            "Our story is unlocked! ♥\n\n" +
+
+            "Happy Anniversary, Love!"
+
+        );
+
+    }
+);
