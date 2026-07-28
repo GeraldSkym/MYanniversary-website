001dc1b26b7a8b6eb9147c3fe655e9b61be54def
GeraldSkym <pestano.gerald16@gmail.com>
2026-07-28 22:39:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const form =
+    document.getElementById("anniversaryForm");
 
+form.addEventListener(
+    "submit",
+    function (event) {
+
+        event.preventDefault();
+
+        const date =
+            document.getElementById(
+                "anniversaryDate"
+            ).value;
+
+        if (!date) {
+
+            alert(
+                "Please enter our anniversary date ♥"
+            );
+
+            return;
+        }
+
+        alert(
+            "Our story is unlocked! ♥\n\n" +
+            "Happy Anniversary, Love!"
+        );
+
+    }
+);
